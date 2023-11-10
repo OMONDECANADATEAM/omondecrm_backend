853bea2b5fcbc50be9e9c1dc5e09672ef98f1e03
Japhet <pierrejaphetkonan@gmail.com>
2023-11-10 12:16:24 +0000

    template html login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{'assets/css/style.css'}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OMONDE | CONNEXION</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <img src="{{'/assets/images/logo-omonde.webp'}}" alt="Logo de votre OMONDE">
+        </div>
+        <div class="menu">
+            <!-- Autres éléments de menu -->
+        </div>
+    </nav>
+    <main>
+         <p>BIENVENUE</p>
+            <form action="">
+               
+                <input class="input-connexion input1" type="text" name="id" id="id" placeholder="Utilisateur">
+               
+                <input class="input-connexion input2" type="text" name="mdp" id="mdp"  placeholder="Mot de passe">
+                <button type="submit" class="btn-connexion" ><a href="pages/home.html" target="_self">CONNEXION</a></button>
+            </form>
+    </main>
+</body>
+</html>
